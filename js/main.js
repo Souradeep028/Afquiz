8c3a7e187a86cebfe3384091a3ff54c2292bb1c5
Souradeep <souradeepkrbasu@gmail.com>
2019-10-25 13:38:57 +0530

    Update

--- js/main.js
+++ js/main.js
@@ -234,12 +234,12 @@ $( document ).ready(function() {
         var o2 = d.outcomes[1];
         var o3 = d.outcomes[2];
         var outcome;
-        if (T>=6) {
+        if (T>=5) {
             $('#endingh').text(o1.heading);
             $('#endingp').text(o1.paragraph);
             $('#guideimg').prop('src', './img/' + city + '/' + o1.img);
             outcome = o1.id;
-        } else if (o2.rr[0].indexOf(q[0]) != -1 && o2.rr[1].indexOf(q[1]) != -1 && o2.rr[2].indexOf(q[2]) != -1 && o1.rr[3].indexOf(q[3]) != -1 && o1.rr[4].indexOf(q[4]) != -1 && o1.rr[5].indexOf(q[5]) != -1 && o1.rr[6].indexOf(q[6]) != -1) {
+        } else if (T==4) {
             $('#endingh').text(o2.heading);
             $('#endingp').text(o2.paragraph);
             $('#guideimg').prop('src', './img/' + city + '/' + o2.img);
